e301b5ff4f6f0e589b4c8e7257e988fbffdf22df
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2015-11-12 22:08:23 +0000

    Add sample to light 5 LEDs in a row, in sequence

---
+++ cycle_leds/cycle_leds.ino
@@ -0,0 +1,22 @@
+int max_pin = 12;
+int min_pin = 8;
+
+int pause_in_msecs = 250;
+
+void setup() {
+  // put your setup code here, to run once:
+  for ( int i = min_pin; i <= max_pin; ++i )
+    pinMode(i, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for ( int i = min_pin; i <= max_pin; ++i )
+  {
+    digitalWrite(i, HIGH);
+    delay(pause_in_msecs);
+    digitalWrite(i, LOW);
+    delay(pause_in_msecs);
+  }
+}
+
